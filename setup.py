48fd6f8f66fb3903cc2567e8dd6fa50b10184324
Aniruddha Pal <aniruddhapal@gmail.com>
2025-02-25 17:44:58 +0530

    Project name changed to Student-Exam-Performance-Prediction and created the project as a package with the same name

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlprojetcs',
+name='Student-Exam-Performance-Prediction',
 version='0.0.1',
 author='Aniruddha Pal',
 author_email='aniruddhapal@gmail.com',
