53ee4c926c16d8c0ee3829a87ea9a22aae453847
Aniruddha Pal <aniruddhapal@gmail.com>
2025-06-08 23:32:42 +0530

    prediction pipeline dated 8th june 2025

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_e_dot='-e .'
+HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function returns the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace('/n',"") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # fixed newline cleanup
 
-        if HYPHEN_e_dot in requirements:
-            requirements.remove(HYPHEN_e_dot)
-    
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
     return requirements
 
 setup(
-name='Student-Exam-Performance-Prediction',
-version='0.0.1',
-author='Aniruddha Pal',
-author_email='aniruddhapal@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+    name='Student-Exam-Performance-Prediction',
+    version='0.0.1',
+    author='Aniruddha Pal',
+    author_email='aniruddhapal@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
